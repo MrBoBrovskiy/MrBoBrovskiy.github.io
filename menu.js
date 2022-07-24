be170052d1184fb16a1cbfed8e5dba2878a17966
MrBoBrovskiy <1thekolobok1@gmail.com>
2022-07-24 16:19:33 +0300

    Почистил мусор, начал делать новое меню

--- menu.js
+++ menu.js
@@ -1,49 +1,40 @@
-// Designed by:  Mauricio Bucardo
-// Original image:
-// https://dribbble.com/shots/5619509-Animated-Tab-Bar
-
-"use strict";
+"use strict"
 
 const body = document.body;
-const bgColorsBody = ["#ffb457", "#ff96bd", "#9999fb", "#ffe797", "#cffff1"];
+const bgColorsBody = ["#3333"];
 const menu = body.querySelector(".menu");
 const menuItems = menu.querySelectorAll(".menu__item");
 const menuBorder = menu.querySelector(".menu__border");
 let activeItem = menu.querySelector(".active");
 
 function clickItem(item, index) {
+const menuItems = menu.querySelectorAll (".menu_item");
+const menuBorder - menu.querySelector (".menu_border")
+let activeItem = menu.querySelector (".active")
 
+function clickItem(item, index){
     menu.style.removeProperty("--timeOut");
-
+    
     if (activeItem == item) return;
-
+    
     if (activeItem) {
         activeItem.classList.remove("active");
     }
-
-
-    item.classList.add("active");
-    body.style.backgroundColor = bgColorsBody[index];
+    
+    item.class.list.add("active");
+    body.style.backgroundColor = brColorsBody[index];
     activeItem = item;
-    offsetMenuBorder(activeItem, menuBorder);
-
-
+    offsetMeniBorder(activeItem, menuBorder);
 }
-
-function offsetMenuBorder(element, menuBorder) {
-
-    const offsetActiveItem = element.getBoundingClientRect();
-    const left = Math.floor(offsetActiveItem.left - menu.offsetLeft - (menuBorder.offsetWidth  - offsetActiveItem.width) / 2) +  "px";
+function offsetMenuBorder(Element, menuBorder) {
+    const offsetActiveItem = Element.getBoundingClientRect();
+    const left = Math.floor(offsetActiveItem.left - menu.offsetLeft - (menuBorder.offsetWidth - offsetActiveItem.width)/2) + "px";
     menuBorder.style.transform = `translate3d(${left}, 0 , 0)`;
-
 }
 
 offsetMenuBorder(activeItem, menuBorder);
-
-menuItems.forEach((item, index) => {
-
-    item.addEventListener("click", () => clickItem(item, index));
-
+menuItems.forEach((item, index) =>{
+    item.addEventListener('click', () => clickItem(item, index));
 })
 
 window.addEventListener("resize", () => {
